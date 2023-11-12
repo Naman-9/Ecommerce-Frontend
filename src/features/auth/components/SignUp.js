b0eaa34461022f07aee283b3fd2854e186ad632d
Naman-9 <namancp7@gmail.com>
2023-11-12 17:18:21 +0530

    build 3

--- src/features/auth/components/SignUp.js
+++ src/features/auth/components/SignUp.js
@@ -19,10 +19,10 @@ export default function SignUp() {
 
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
+        <img
             className="mx-auto h-10 w-auto"
             src="./ecommerce.png"
-            alt="Ecom"
+            alt="Ecom Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Register Your  account
